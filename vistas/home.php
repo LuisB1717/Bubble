9424b67f070c872672a3111571d19e0e25063a9a
ChristianTSC <ctorressantacruz98@gmail.com>
2021-09-28 10:08:05 -0500

    avanceCSS-Home

---
+++ vistas/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+	<title>INGRESO DE DATOS</title>
+    <link rel="stylesheet" href="principal.css">
+    
+</head>
+<body class="fondo">
+    <div align="center"><img  src="bubble.png" ></div>
+    <div align="center">
+        <a class="boton" href="" >Cliente</a>
+        <a class="boton" href="" >Negocio</a>
+    </div>
+     
+
+    
+	
+
+</body>
+</html>
