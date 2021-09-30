19c019dd8a0476310fdbd05516cd3d32e0640dd3
Luis <luisbarboz17@gmail.com>
2021-09-30 00:38:29 -0500

    vista_cliente&negocio

--- publico/auth/index.js
+++ publico/auth/index.js
@@ -1,7 +1,11 @@
 import firebase from "../dependencias/firebase.js";
 
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+
 async function iniciarSesion(){
-  console.log('Iniciando sesión');
+  console.log('Iniciando sesión para ',params.tipo);
   const resultado = await firebase.auth.iniciarSesionGoogle()
   console.log('Inicio sesión', resultado);
 }
