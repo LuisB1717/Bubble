9d9c4630dc1059be3d52daa814c5d54dc9fa83a2
Luis <luisbarboz17@gmail.com>
2021-10-08 00:23:12 -0500

    ...

--- publico/cliente/chat.js
+++ publico/cliente/chat.js
@@ -1,16 +1,13 @@
 
 function mostrar_chats(){
-
+    
     $.ajax({
         url:'lista_chats.html',
-
         beforesed:function(){
             $('#chat_lista').html("Cargando...");
         },
         success: function (mensaje_mostar){
             $('#chat_lista').html(mensaje_mostar);
         }
-
-
     })
 }
