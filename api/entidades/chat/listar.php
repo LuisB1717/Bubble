9978ffa628d05fdded4d98467881e470d57eb39b
ChristianTSC <ctorressantacruz98@gmail.com>
2021-10-31 18:46:57 -0500

    chat php

---
+++ api/entidades/chat/listar.php
@@ -0,0 +1,14 @@
+<?php
+include("../../dependencias/db/conexión.php");
+$idCliente=$_GET['idCliente']
+
+$sql ="SELECT * FROM chat where idCliente=$idCliente"; 
+$resultado=mysqli_query($conexion,$sql);
+$datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
+
+if(!empty($datos)){
+    echo json_encode($datos);
+}else{
+    echo json_encode([]);
+}
+?>
